51b7fb93d228be8adbb7a62e9bfbfad93ce8af3c
williammc <williammc@10.250.18.53>
2017-01-26 12:03:11 -0500

    This is for a test

--- src/demotest/week2/DemoTestWeek2.java
+++ src/demotest/week2/DemoTestWeek2.java
@@ -16,7 +16,7 @@ public class DemoTestWeek2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        System.out.println("Hello World PART 2!!asfsaafadsfds");
     }
     
 }
